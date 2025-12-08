1cac26e9d837ffcf9de7eafe90311b0c0a2969d4
pelu29 <floresjack74@gmail.com>
2025-12-08 17:05:50 -0500

    Configuración de proyecto inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { Nosotros } from './components/nosotros/nosotros';
+import { Servicios } from './components/servicios/servicios';
+import { Contacto } from './components/contacto/contacto';
+import { Blog } from './components/blog/blog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'inicio',pathMatch:'full'},
+    {path:'inicio',component:Inicio},
+    {path: 'nosotros',component:Nosotros},
+    {path: 'servicios',component:Servicios},
+    {path: 'contacto', component:Contacto},
+    {path: 'blog',component:Blog}
+];
